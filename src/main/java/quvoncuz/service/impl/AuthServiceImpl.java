a94503156f07984d44062cecd5fbb3175fbcd506
quvoncuz <quvoncbe.uz@gmail.com>
2026-03-30 15:33:07 +0500

    Auth

---
+++ src/main/java/quvoncuz/service/impl/AuthServiceImpl.java
@@ -0,0 +1,70 @@
+package quvoncuz.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import quvoncuz.dto.auth.AuthResponse;
+import quvoncuz.entities.ProfileEntity;
+import quvoncuz.exceptions.AlreadyExistsException;
+import quvoncuz.dto.auth.LoginRequestDTO;
+import quvoncuz.dto.ProfileDTO;
+import quvoncuz.dto.auth.RegistrationRequestDTO;
+import quvoncuz.exceptions.DoNotMatchException;
+import quvoncuz.exceptions.InvalidException;
+import quvoncuz.mapper.ProfileMapper;
+import quvoncuz.service.AuthService;
+import quvoncuz.service.ProfileService;
+
+import java.util.Base64;
+import java.util.regex.Pattern;
+
+@RequiredArgsConstructor
+public class AuthServiceImpl implements AuthService {
+
+    private final ProfileService profileService;
+    private static final String EMAIL_REGEX = "^[a-zA-Z0-9._%+\\-]+@[a-zA-Z0-9.\\-]+\\.[a-zA-Z]{2,}$";
+    private static final Pattern pattern = Pattern.compile(EMAIL_REGEX);
+
+    @Override
+    public AuthResponse register(RegistrationRequestDTO dto) {
+        if (profileService.existsByEmail(dto.getEmail())) {
+            throw new AlreadyExistsException("Email already exists!");
+        }
+        if (profileService.existsByUsername(dto.getUsername())) {
+            throw new AlreadyExistsException("Username already exists!");
+        }
+        if (!isValidEmail(dto.getEmail())){
+            throw new InvalidException("Invalid email");
+        }
+
+
+        ProfileEntity profile = profileService.create(dto);
+
+        String token = Base64.getEncoder().encodeToString((profile.getUsername() + ":" + profile.getPassword()).getBytes());
+
+        return new AuthResponse(
+                 profile.getUsername(),
+                profile.getRole().name(),
+                profile.getId());
+
+    }
+
+    @Override
+    public AuthResponse login(LoginRequestDTO dto) {
+        ProfileEntity profile = profileService.findByUsername(dto.getUsername());
+        if (!profile.getPassword().equals(dto.getPassword())){
+            throw new DoNotMatchException("Username or password incorrect");
+        }
+        if (!profile.getIsActive()){
+            throw new InvalidException("Profile is not active");
+        }
+
+        return new AuthResponse(
+                profile.getUsername(),
+                profile.getRole().name(),
+                profile.getId());
+    }
+
+    private boolean isValidEmail(String email) {
+        if (email == null) return false;
+        return pattern.matcher(email).matches();
+    }
+}
