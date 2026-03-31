cf47c81fd7674e59c659cdf89f3e1a3f2e648892
quvoncuz <quvoncbe.uz@gmail.com>
2026-03-31 09:26:28 +0500

    paid amount added

--- src/main/java/quvoncuz/entities/BookingEntity.java
+++ src/main/java/quvoncuz/entities/BookingEntity.java
@@ -1,18 +1,23 @@
 package quvoncuz.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import quvoncuz.enums.BookingStatus;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookingEntity {
 
     private Long id;
     private Long userId;
     private Long tourId;
     private Integer seatsBooked;
+    private BigDecimal paidAmount;
     private BigDecimal totalPrice;
     private BookingStatus status;
     private String note;
