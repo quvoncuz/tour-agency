ea08d4c15e058781140f96fbe17737c61c45a8d9
quvoncuz <quvoncbe.uz@gmail.com>
2026-04-08 16:50:43 +0500

    spring configuration modified and swagger added to maven

--- src/main/java/quvoncuz/config/SpringConfig.java
+++ src/main/java/quvoncuz/config/SpringConfig.java
@@ -1,7 +1,21 @@
 package quvoncuz.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import tools.jackson.databind.ObjectMapper;
 
 @Configuration
 public class SpringConfig {
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper(){
+        return new ObjectMapper();
+    }
+
 }
