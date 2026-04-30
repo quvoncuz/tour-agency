1627fe8cd56ac482d2ecb31636401461e86bb4af
quvoncuz <quvoncbe.uz@gmail.com>
2026-04-30 16:25:57 +0500

    new migration added

--- src/main/resources/db/migration/sql/tables/values/v1_009_user_init_value.sql
+++ src/main/resources/db/migration/sql/tables/values/v1_009_user_init_value.sql
@@ -2,7 +2,7 @@
 
 -- changeset dev:user_init_values
 insert into profiles(id, full_name, username, email, password, role, gender, is_created_agency, is_active, created_at)
-values(1, 'Admin', 'admin', 'admin@gmail.com', 'Admin123', 'ADMIN', 'MALE', true, true, now()),
-      (2, 'Quvonchbek', 'quvonc', 'quvonc@gmail.com', '123', 'USER', 'MALE', false, true, now()),
-      (3, 'Quvonchbek', 'quvonch', 'quvonch@gmail.com', '123', 'AGENCY', 'MALE', true, true, now())
+values(1, 'Admin', 'admin', 'admin@gmail.com', '$2a$12$tNFAcQ9jWFm5ItPv7XQ//.sYlgNy1Y2PKqtZjszesj/Gxm9/Z4TOi', 'ADMIN', 'MALE', true, true, now()),
+      (2, 'Quvonchbek', 'quvonc', 'quvonc@gmail.com', '$2a$12$6EktIGAL1NcTZMHwiZVaMeVLJzlUxkPrF6xxIPdjwCdD.3EH3s2R.', 'USER', 'MALE', false, true, now()),
+      (3, 'Quvonchbek', 'quvonch', 'quvonch@gmail.com', '$2a$12$QxiEjqe1YXYN8eEJxLchz./6nxX/bEPk9dV10En3bxNl7ySiDd4HK', 'AGENCY', 'MALE', true, true, now())
 on conflict (id) do nothing
