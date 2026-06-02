94917de98fb93235f7280485247cfa1c03183d4e
quvoncuz <quvoncbe.uz@gmail.com>
2026-06-02 14:19:59 +0500

    Events created

---
+++ src/main/java/quvoncuz/events/StatisticsEvent.java
@@ -0,0 +1,24 @@
+package quvoncuz.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import quvoncuz.enums.EventType;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StatisticsEvent {
+
+    private Long superId; // agencyId <- tourId,   tourId <- bookingId
+
+    private Long entityId;
+
+    private EventType eventType;
+
+    private LocalDateTime dateTime;
+}
