d2bc89dfe1ecad6a27e8ec8369e40419ec041a31
quvoncuz <quvoncbe.uz@gmail.com>
2026-06-26 16:38:13 +0500

    testcontainers

--- src/test/java/quvoncuz/integration/BaseIntegrationTest.java
+++ src/test/java/quvoncuz/integration/BaseIntegrationTest.java
@@ -2,18 +2,15 @@ package quvoncuz.integration;
 
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
-@Transactional
 @Testcontainers
 public abstract class BaseIntegrationTest {
 
@@ -23,20 +20,21 @@ public abstract class BaseIntegrationTest {
     }
 
     @Container
-    static PostgreSQLContainer<?> postgres =
-            new PostgreSQLContainer<>("postgres:15")
+    @ServiceConnection // 💡 Spring Boot buni o'zi DataSource konfiguratsiyasiga ulaydi
+    protected static final PostgreSQLContainer<?> postgresContainer =
+            new PostgreSQLContainer<>("postgres:15-alpine")
                     .withDatabaseName("testdb")
                     .withUsername("test")
                     .withPassword("test");
 
-    @DynamicPropertySource
-    static void properties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
-        registry.add("spring.datasource.driver-class-name",
-                () -> "org.postgresql.Driver");
-        registry.add("spring.jpa.database-platform",
-                () -> "org.hibernate.dialect.PostgreSQLDialect");
-    }
+//    @DynamicPropertySource
+//    static void properties(DynamicPropertyRegistry registry) {
+//        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+//        registry.add("spring.datasource.username", postgres::getUsername);
+//        registry.add("spring.datasource.password", postgres::getPassword);
+//        registry.add("spring.datasource.driver-class-name",
+//                () -> "org.postgresql.Driver");
+//        registry.add("spring.jpa.database-platform",
+//                () -> "org.hibernate.dialect.PostgreSQLDialect");
+//    }
 }
